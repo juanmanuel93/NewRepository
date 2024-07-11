870d6a9ee2c567709384d427953e6d9f81978f1a
Juan Manuel <juan.arenas@datafaith.co>
2024-07-11 15:01:16 -0500

    w

--- tutorial.js
+++ tutorial.js
@@ -1,30 +1,34 @@
 import pg from "pg";
-import http from "http";
+import https2 from "https";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const pool = new pg.Pool({
     host: "localhost",
     port: 5432,
     database: "NCPilot",
     user: "postgres",
-    password: "NC1234"
+    password: process.env.PASSWORD
 });
 
 var result = "No data";
 const getData = async () => {
-try {
     result = await pool.query('select first_name, last_name, primary_email from "NCPilotDB".subscribers where last_name = \'Simpson\';');
     console.log(result.rows);
-    return result;
-}
-catch(error){
-    return error;
-}
 };
 
 getData();
 
-var data = http.createServer(function (req, res) {
+const options = {};
+  
+  https2.createServer(options, (req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end(JSON.stringify(result.rows));
-}).listen(8080);
+  }).listen(8080);
+
+// var data = https.createServer(function (req, res) {
+//     res.writeHead(200, {'Content-Type': 'text/plain'});
+//     res.end(JSON.stringify(result.rows));
+// }).listen(8080);
     
